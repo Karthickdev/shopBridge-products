fa2926ce8414e4eaf68633ab1a90f7aa952b8857
Karthickdev <46392745+Karthickdev@users.noreply.github.com>
2021-04-23 16:03:22 +0530

    added httpModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { EditModalComponent } from './edit-modal/edit-modal.component';
 import { FormsModule } from '@angular/forms';
 import { AddModalComponent } from './add-modal/add-modal.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { AddModalComponent } from './add-modal/add-modal.component';
     MatDividerModule,
     MatListModule,
     MatIconModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
